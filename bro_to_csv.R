758f6be575daa5a89d8ac8492a43f04023280f4b
hk21042 <43421964+hk21042@users.noreply.github.com>
2018-11-10 19:10:17 -0500

    Add files via upload
    code now translates uploaded bro file into csv file with complete table of data. also writes csv file to working directory (column names included).

--- bro_to_csv.R
+++ bro_to_csv.R
@@ -1,23 +1,40 @@
 library("stringr")
+setwd("/Volumes/HANNAH/TDA/Threat Hunt")
 og_file <- file.choose()
 bro_file <- data.frame(readLines(og_file), stringsAsFactors = FALSE, drop = FALSE)
 
 #deletes all the headers at the top
 to_delete <- c()
 for (i in 1:nrow(bro_file)){
-  if (substring(bro_file[i,1],1,1)=="#" && substring(bro_file[i,1],1,7) != "#fields"){
+  if (substring(bro_file[i,1],1,1)=="#" && substring(bro_file[i,1],1,8) != "#fields\t"){
     to_delete <- append(to_delete, i)
   }
 }
 bro_file <- bro_file[-to_delete,]
 
-bro_file[1,1] <- gsub("#fields", "", bro_file[1,1])
+#starts creating new data frame
+num.rows <- nrow(bro_file)
+csv.df <- data.frame()
 
-#to fix: x is correct but doesn't transfer into data frame correctly. 
-new.row <- gsub("\t", " ", bro_file[1,1])
+#splits data by whitespace and enters each term into column
+for (i in 2:num.rows){
+  new.row <- str_split(bro_file[i,1], "\t")
+  new.row <- as.character(unlist(new.row, use.names=FALSE))
+  x <- f4(new.row)
+  new.df <- t(data.frame(x, stringsAsFactors=FALSE))
+  csv.df <- rbind(csv.df, new.df)
+}
+
+#creates column rows from #fields row in bro file
+bro_file[1,1] <- toString(gsub("#fields\t", "", bro_file[1,1]))
+new.row <- str_split(bro_file[1,1], "\t")
+new.row <- as.character(unlist(new.row, use.names=FALSE))
 x <- f4(new.row)
-data.frame(x, stringsAsFactors=FALSE)
+colnames(csv.df) <- as.character(x)
 
+write.csv(csv.df, "csv data.csv", row.names = FALSE)
+
+#convert into list
 f4 <- function(n) {
   x <- character(length(n))
   for (i in 1:length(n)) {
