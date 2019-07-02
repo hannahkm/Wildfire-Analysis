c1c4daaa5abd029c520f62b21df87852daef09d9
kimh2 <Hannah.Kim@jhuapl.edu>
2019-07-02 17:14:18 -0400

    not much
    not much changed. working on returning a df with the cluster each row is contained in. to do: find a work around for points that are in multiple clusters.

--- printing_points.R
+++ printing_points.R
@@ -1,8 +1,8 @@
 setwd(
   "/Users/hk/Desktop/School/MRHS/11th\ Grade/R/NN-ML/Wildfire-NN-ML/ML_Data/Old\ Data"
 )
-data <- read.csv("merra2_active_calfire_jja.csv")[, c("t2mmax", "qv2m",
-                                                "fcount_aqua", "year", "month", "day")] #2,8
+data <- read.csv("C:/Users/kimh2/Desktop/Wildfire-NN-ML/ML_Data/Old Data/merra2_active_calfire_jja.csv")[, 
+                              c("t2mmax", "qv2m", "fcount_aqua", "year", "month", "day")] #2,8
 
 data.dist <- dist(data[, c(1, 2)])
 par(mfrow = c(1, 1))
@@ -20,6 +20,10 @@ data.graph <-
 
 vertex.size <- rep(0, data.mapper2$num_vertices)
 df <- as.data.frame(matrix(ncol = 3))
+values <- rep(".", nrow(data))
+data <- cbind(data, values)
+colnames(data)[[ncol(data)]] <- "cluster"
+
 for (i in 1:data.mapper2$num_vertices) {
   points.in.vertex <- data.mapper2$points_in_vertex[[i]]
   len <- length(points.in.vertex)
@@ -27,16 +31,19 @@ for (i in 1:data.mapper2$num_vertices) {
   for (j in 1:len) {
     if (!is.na(data[points.in.vertex[[j]], 3])) {
       count <- count + abs(data[points.in.vertex[[j]], 3])
-      cat("vertex (i): ", i, " point # (j): ", j, " y-m-d: ", data[points.in.vertex[[j]],4],"-",
-          data[points.in.vertex[[j]],5], "-", data[points.in.vertex[[j]],6], "\n")
+      # cat("vertex (i): ", i, " point # (j): ", j, " y-m-d: ", data[points.in.vertex[[j]],4],"-",
+      #     data[points.in.vertex[[j]],5], "-", data[points.in.vertex[[j]],6], "\n")
       df_add <-
         as.data.frame(cbind(data[points.in.vertex[[j]], 4], data[points.in.vertex[[j]], 5],
                             data[points.in.vertex[[j]], 6]))
       df <- rbind(df, df_add)
+      
+      print(data[points.in.vertex[[j]],ncol(data)])
+      data[points.in.vertex[[j]],ncol(data)] <- (paste0(data[points.in.vertex[[j]],ncol(data)], i))
     }
-    else if (is.na(data[points.in.vertex[[j]], 3]) && j == 1) {
-      cat("vertex: ", i, " empty", "\n")
-    }
+    # else if (is.na(data[points.in.vertex[[j]], 3]) && j == 1) {
+    #   cat("vertex: ", i, " empty", "\n")
+    # }
     
   }
   
@@ -47,3 +54,4 @@ for (i in 1:data.mapper2$num_vertices) {
 df <- unique(df[order(df[, 1], df[, 2], df[, 3]), ])
 df <- df[-1,]
 print(nrow(df))
+
--- tda_with_functions.R
+++ tda_with_functions.R
@@ -22,8 +22,8 @@ setwd("/Volumes/HKIM/TDA/")
 #setwd("E:\\TDA")
 setwd("/Users/hk/Desktop/School/MRHS/11th\ Grade/R/NN-ML/Wildfire-NN-ML/ML_Data/Old\ Data")
 
-tmax_humidity <- read.csv("merra2_active_calfire_jja.csv")[,c("t2mmax", "qv2m", 
-                                    "fcount_aqua","fwi","isi","bui","dc","speed","t2m")] #2,8
+tmax_humidity <- read.csv("C:/Users/kimh2/Desktop/Wildfire-NN-ML/ML_Data/Old Data/merra2_active_calfire_jja.csv")[,
+                  c("t2mmax", "qv2m", "fcount_aqua","fwi","isi","bui","dc","speed","t2m")] #2,8
 if (true){
   tmax_humidity_y <- read.csv("merra2_inactive_calfire_jja.csv")[,c("t2mmax", "qv2m",
                                                                     "fcount_aqua")]
@@ -403,47 +403,50 @@ printList <- function(list) {
 }
 
 returnData <- function(data){
-  par(mfrow=c(1,1), mar=c(2,3,2,2))
+  par(mfrow = c(1, 1))
   
-  data.mapper2 <- mapper2D(
-    distance_matrix = dist(data.frame( x=data[,1], y=data[,2] )),
+  data.mapper2 <- mapper2D( 
+    distance_matrix = dist(data.frame(x = data[, 1], y = data[, 2])),
     filter_values = list(data[,1],data[,2]),
-    num_intervals = c(5,5),
+    num_intervals = c(5, 5),
     percent_overlap = 60,
-    num_bins_when_clustering = 60)
+    num_bins_when_clustering = 60
+  )
   
-  df <- data.frame(matrix())
+  data.graph <-
+    graph.adjacency(data.mapper2$adjacency, mode = "undirected")
   
-  for (i in 1:data.mapper2$num_vertices){
-    df <- rbind.fill(df,as.data.frame(t(data.mapper2$points_in_vertex[[i]])))
-  }
-  df <- df[-1,]
-  
-  vertex.size <- rep(0,data.mapper2$num_vertices)
-  
-  for (i in 1:data.mapper2$num_vertices){
-    for (j in 1:length(data.mapper2$points_in_vertex[[i]])){
-      vertex.size[i] <- vertex.size[i] + data[data.mapper2$points_in_vertex[[i]][j],3]
+  vertex.size <- rep(0, data.mapper2$num_vertices)
+  df <- as.data.frame(matrix(ncol = 3))
+  for (i in 1:data.mapper2$num_vertices) {
+    points.in.vertex <- data.mapper2$points_in_vertex[[i]]
+    len <- length(points.in.vertex)
+    count <- 0
+    for (j in 1:len) {
+      if (!is.na(data[points.in.vertex[[j]], 3])) {
+        count <- count + abs(data[points.in.vertex[[j]], 3])
+        cat("vertex (i): ", i, " point # (j): ", j, " y-m-d: ", data[points.in.vertex[[j]],4],"-",
+            data[points.in.vertex[[j]],5], "-", data[points.in.vertex[[j]],6], "\n")
+        df_add <-
+          as.data.frame(cbind(data[points.in.vertex[[j]], 4], data[points.in.vertex[[j]], 5],
+                              data[points.in.vertex[[j]], 6]))
+        df <- rbind(df, df_add)
+      }
+      else if (is.na(data[points.in.vertex[[j]], 3]) && j == 1) {
+        cat("vertex: ", i, " empty", "\n")
+      }
+      
     }
+    
+    vertex.size[i] <- count
+    
   }
   
-  vertex.size <- (scale(vertex.size)+1)*5
-  df[,1] <- vertex.size
-  df[is.na(df)] <- "."
-  
-  View(df)
-  print(sort(unique(as.vector(as.matrix(df[,-1])))))
+  df <- unique(df[order(df[, 1], df[, 2], df[, 3]), ])
+  df <- df[-1,]
+  df
   
 }
-
-#mapper performs clustering within each level set and generate 
-    #one node (vertex) for each cluster
-
-#interesting: For each pair of clusters in adjacent level sets with a nonempty 
-    #intersection, generate one edge between vertices. The function 
-    #mapper1D uses a filter function with codomain R, while the 
-    #function mapper2D uses a filter function with codomain R^2.
-
 
 
 
